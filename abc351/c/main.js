312932770d304dd5a2c4770f1aa2cd32b4e132ba
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-08-31 21:23:54 +0900

    351 c clear

--- abc351/c/main.js
+++ abc351/c/main.js
@@ -1,2 +1,26 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
+
+const numbers = lines[1].split(' ').map(Number);
+
+const stack = [];
+
+for (const num of numbers) {
+  if (stack.length === 0) {
+    stack.push(num);
+    continue;
+  }
+  stack.push(num);
+  if (stack[stack.length - 1] !== stack[stack.length - 2]) {
+    continue;
+  } else {
+    while (stack[stack.length - 1] === stack[stack.length - 2]) {
+      const a = stack[stack.length - 1];
+      stack.splice(-2);
+
+      stack.push(a + 1);
+    }
+  }
+}
+
+console.log(stack.length);
