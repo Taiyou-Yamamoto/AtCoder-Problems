8e6539575625dd8af62dcf761738ba659cc2240f
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-09-07 23:11:13 +0900

    352 c

--- abc352/c/main.js
+++ abc352/c/main.js
@@ -1,2 +1,19 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
+
+const number = Number(lines[0]);
+
+let total = 0;
+let maxDiff = 0;
+
+for (let i = 1; i <= number; i++) {
+  const [a, b] = lines[i].split(' ').map(Number);
+
+  total += a;
+  const diff = b - a;
+  if (diff > maxDiff) {
+    maxDiff = diff;
+  }
+}
+
+console.log(total + maxDiff);
