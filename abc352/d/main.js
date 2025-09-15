47c2754e32798e8592da7ac5b7c62a3599ba931b
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-09-15 14:41:20 +0900

    fix

--- abc352/d/main.js
+++ abc352/d/main.js
@@ -20,12 +20,12 @@ let maxHead = 0;
 let minHead = 0;
 
 for (let i = 0; i < length; i++) {
-  while (maxQ.length && trimmed[maxQ[maxQ.length - 1]] <= trimmed[i]) {
+  while (maxQ.length > maxHead && trimmed[maxQ[maxQ.length - 1]] <= trimmed[i]) {
     maxQ.pop();
   }
   maxQ.push(i);
 
-  while (minQ.length && trimmed[minQ[minQ.length - 1]] >= trimmed[i]) {
+  while (minQ.length > minHead && trimmed[minQ[minQ.length - 1]] >= trimmed[i]) {
     minQ.pop();
   }
   minQ.push(i);
@@ -33,13 +33,13 @@ for (let i = 0; i < length; i++) {
   if (i >= pick - 1) {
     const l = i - (pick - 1);
 
-    // 窓の外（lより左）を先頭ポインタを進めて捨てる（shiftしない！）
-    while (maxQ[maxHead] < l) maxHead++;
-    while (minQ[minHead] < l) minHead++;
+    // 窓の外（lより左）を先頭ポインタを進めて捨てる（shiftしない）
+    while (maxHead < maxQ.length && maxQ[maxHead] < l) maxHead++;
+    while (minHead < minQ.length && minQ[minHead] < l) minHead++;
 
-    const width = B[maxQ[maxHead]] - B[minQ[minHead]];
+    const width = trimmed[maxQ[maxHead]] - trimmed[minQ[minHead]];
     if (width < ans) ans = width;
   }
 }
 
-console.log(ans.toString());
+console.log(ans);
