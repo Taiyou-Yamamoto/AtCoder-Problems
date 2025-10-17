19c24991b474d5c3468f68e9ad14148556ac4d4b
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-10-17 09:20:38 +0900

    add

--- abc195/b/main.js
+++ abc195/b/main.js
@@ -1,2 +1,13 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
+
+const [A, B, W] = lines[0].split(' ').map(Number);
+
+const upper = Math.floor((W * 1000) / A);
+const lower = Math.ceil((W * 1000) / B);
+
+if (upper < lower) {
+    console.log('UNSATISFIABLE');
+} else {
+    console.log(`${lower} ${upper}`);
+}
