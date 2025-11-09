d55b9fc82725bb34781858f3d4f656122803690a
山本　太陽 <yamamototaiyou.811@gmail.com>
2025-11-09 22:56:17 +0900

    fix

--- abc068/c/main.js
+++ abc068/c/main.js
@@ -1,2 +1,21 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.trim().split('\n');
+const fs = require('fs');
+
+const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
+let idx = 0;
+const islands = input[idx++];
+const seaRoutes = input[idx++];
+
+const graph = Array.from({ length: islands }, () => []);
+
+for (let i = 0; i < seaRoutes; ++i) {
+    const a = input[idx++];
+    const b = input[idx++];
+
+    graph[a - 1].push(b);
+    graph[b - 1].push(a);
+}
+
+const start = new Set(graph[0]);
+const isPossible = graph[islands - 1].some((v) => start.has(v));
+
+console.log(isPossible ? 'POSSIBLE' : 'IMPOSSIBLE');
